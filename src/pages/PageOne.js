97d3cf8c42a3130fff08efb15a8704472a5bc6c6
joboot <jordan.booth41900@gmail.com>
2022-12-05 09:15:05 -0600

    Installed some modules

--- src/pages/PageOne.js
+++ src/pages/PageOne.js
@@ -5,11 +5,7 @@ import PDFViewer from 'pdf-viewer-reactjs'
 export default class PageOne extends Component {
   render() {
     return (
-      <PDFViewer
-      document={{
-          url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
-      }}
-  />
+      <div>Page One</div>
     )
   }
 }
