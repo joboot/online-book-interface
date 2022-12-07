53220bb33d37c6b393cfb0ef091b8eaf161dbdf9
joboot <jordan.booth41900@gmail.com>
2022-12-07 12:05:50 -0600

    commented out bad code

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 // import Header from './TopBarComp'
 import Sidebar from './SidebarComp'
 import PageOne from '../pages/PageOne'
+import SignIn from '../pages/SignIn'
 
 
 export default class DefaultPage extends Component {
@@ -16,8 +17,9 @@ export default class DefaultPage extends Component {
                 </Col>
             </Row>
             <Row>
-                <Col md="auto"><Sidebar/></Col>
-                <Col md="auto"><PageOne/></Col>
+                {/* <Col md="auto"><Sidebar/></Col>
+                <Col md="auto"><PageOne/></Col> */}
+                <Col md="auto"><SignIn/></Col>
             </Row>
         </Container>
       </div>
