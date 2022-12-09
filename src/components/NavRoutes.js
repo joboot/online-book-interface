5a5e8b25c92242fba7f722cb486229d39ef24ea0
joboot <jordan.booth41900@gmail.com>
2022-12-09 12:50:19 -0600

    Navigation working

--- src/components/NavRoutes.js
+++ src/components/NavRoutes.js
@@ -7,12 +7,13 @@ import {
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import PageOne from '../pages/PageOne';
+import DefaultPage from './DefaultPage';
 
 const NavRoutes = () => (
   <Routes>
-    <Route exact path="/" component={SignIn} />
-    <Route exact path="/Home" component={Home} />
-    <Route exact path="/PageOne" component={PageOne} />
+    <Route exact path="/" element={<DefaultPage Page = {<SignIn/>}/>} />
+    <Route exact path="/Home" element={<DefaultPage Page = {<Home/>}/>} />
+    <Route exact path="/PageOne" element={<DefaultPage Page = {<PageOne/>}/>} />
   </Routes>
 );
 
