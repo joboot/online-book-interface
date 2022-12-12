629ebd3721a202c46988b6047647b5b00d6f40b2
joboot <jordan.booth41900@gmail.com>
2022-12-12 12:21:01 -0600

    Moved nav routes to the navroutes.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import Home from './pages/Home';
 import DefaultPage from './components/DefaultPage';
 import NavRoutes from './components/NavRoutes';
 import Footer from './components/FooterComp';
-import Title from './pages/book/title'
-import Intro from './pages/book/intro'
-import Ch1Page1 from './pages/book/chapter1p1';
-import Ch1Page2 from './pages/book/chapter1p2';
-import Ch2Page1 from './pages/book/chapter2p1';
-import Ch2Page2 from './pages/book/chapter2p2';
+
 
 function App() {
   return (
@@ -21,12 +16,7 @@ function App() {
       <Router> 
           <NavRoutes/>
           <Routes>
-            <Route path='/title' exact element={<Title/>} />
-            <Route path='/intro' element={<Intro/>} />
-            <Route path='/chapter1p1' element={<Ch1Page1/>} />
-            <Route path='/chapter1p2' element={<Ch1Page2/>} />
-            <Route path='/chapter2p1' element={<Ch2Page1/>} />
-            <Route path='/chapter2p2' element={<Ch2Page2/>} />
+            
           </Routes>
           <Footer /> {/* Footer component */}
       </Router>
