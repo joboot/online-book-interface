2e9f2d11e2f4c032dd14dd4ddefdcdcdb66c082b
joboot <jordan.booth41900@gmail.com>
2022-12-12 12:55:05 -0600

    Working on default page layout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import Sidebar from './components/SidebarComp';
 import SignIn from './pages/SignIn';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from "react";
-import PageOne from './pages/PageOne';
-import Home from './pages/Home';
-import DefaultPage from './components/DefaultPage';
 import NavRoutes from './components/NavRoutes';
-import Footer from './components/FooterComp';
+
 
 
 function App() {
@@ -15,10 +12,6 @@ function App() {
     <div className="App"> 
       <Router> 
           <NavRoutes/>
-          <Routes>
-            
-          </Routes>
-          <Footer /> {/* Footer component */}
       </Router>
     </div>
   );
