86ba79067a2c1f6f58fc13dce029c5a2c49f37ca
jacarp <91710233+jacarp@users.noreply.github.com>
2022-12-13 13:41:19 -0600

    Reformatted subsubnav

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -14,102 +14,52 @@ export const SidebarData = [
 	iconOpened: <RiIcons.RiArrowUpSFill />,
 },
 {
-	title: "Textbook",
+	title: "Textbook Intro",
+		path: "/intro",
+		icon: <IoIcons.IoIosPaper />,
+},
+{
+	title: "Chapter 1: Hello World",
 	icon: <IoIcons.IoIosPaper />,
 	iconClosed: <RiIcons.RiArrowDownSFill />,
 	iconOpened: <RiIcons.RiArrowUpSFill />,
-
 	subNav: [
 	{
-		title: "Title",
-		path: "/title",
+		title: "Languages",
+		path: "/chapter1p1",
 		icon: <IoIcons.IoIosPaper />,
 		cName: "sub-nav",
 	},
 		
 	{
-		title: "Intro",
-		path: "/intro",
+		title: "\"Hello World\" Program",
+		path: "/chapter1p2",
 		icon: <IoIcons.IoIosPaper />,
 		cName: "sub-nav",
 	},
-	{
-		title: "Chapter 1: Hello World",
-		path: "/chapter1p1",
-		icon: <IoIcons.IoIosPaper />,
-		iconClosed: <RiIcons.RiArrowDownSFill />,
-		iconOpened: <RiIcons.RiArrowUpSFill />,
-		cName: "sub-nav",
-
-		subNav: [
-			{
-				title: "Being able to code",
-				path: "/chapter1p1",
-				icon: <IoIcons.IoIosPaper />,
-				cName: "sub-nav",
-			},
-			{
-				title: "\"Hello World\"",
-				path: "/chapter1p2",
-				icon: <IoIcons.IoIosPaper />,
-				cName: "sub-nav",
-			}
-		]
-	},
-	{
-		title: "Chapter 2: Tips and Tricks",
-		path: "/chapter2p1",
-		icon: <IoIcons.IoIosPaper />,
-		iconClosed: <RiIcons.RiArrowDownSFill />,
-		iconOpened: <RiIcons.RiArrowUpSFill />,
-
-		subNav: [
-			{
-				title: "Rubber Duck Technique",
-				path: "/chapter1p1",
-				icon: <IoIcons.IoIosPaper />,
-				cName: "sub-nav",
-			},
-			{
-				title: "Semicolons and Other Problems",
-				path: "/chapter1p2",
-				icon: <IoIcons.IoIosPaper />,
-				cName: "sub-nav",
-			}
-		]
-	},
-	],
+	]
 },
-/*
 {
-	title: "Contact",
-	path: "/contact",
-	icon: <FaIcons.FaPhone />,
-},
-*/
-/*
-{
-	title: "Events",
-	path: "/events",
-	icon: <FaIcons.FaEnvelopeOpenText />,
-
+	title: "Chapter 2: Tips and Tricks",
+	icon: <IoIcons.IoIosPaper />,
 	iconClosed: <RiIcons.RiArrowDownSFill />,
 	iconOpened: <RiIcons.RiArrowUpSFill />,
 
 	subNav: [
-	{
-		title: "Event 1",
-		path: "/events/events1",
-		icon: <IoIcons.IoIosPaper />,
-	},
-	{
-		title: "Event 2",
-		path: "/events/events2",
-		icon: <IoIcons.IoIosPaper />,
-	},
-	],
+		{
+			title: "Debugging 101",
+			path: "/chapter2p1",
+			icon: <IoIcons.IoIosPaper />,
+			cName: "sub-nav",
+		},
+		{
+			title: "Rubber Duck Technique",
+			path: "/chapter2p2",
+			icon: <IoIcons.IoIosPaper />,
+			cName: "sub-nav",
+		}
+	]
 },
-*/
 {
 	title: "Support",
 	path: "/SupportPage",
